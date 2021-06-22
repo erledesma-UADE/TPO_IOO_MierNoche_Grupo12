16f68f1095d7da7d4a3f0f85d39c99ae046bde2b
juliantoledo94 <juliantoledo94@hotmail.com>
2021-06-22 19:17:29 -0300

    retencion
    completa con getters y setters

---
+++ TPO_IOO_MierNoche_Grupo12/TPOIOOMierNocheGrupo12/src/models/domain/Retencion.java
@@ -0,0 +1,42 @@
+package models.domain;
+
+public class Retencion {
+    private int idRetencion;
+    private Impuesto impuesto;
+    private float monto;
+
+    public Retencion(int idRetencion, Impuesto impuesto, float monto) {
+            this.idRetencion = idRetencion;
+            this.impuesto = impuesto;
+            this.monto = monto;
+        }
+
+    public void setIdRetencion(int idRetencion) {
+        this.idRetencion = idRetencion;
+    }
+
+    public void setImpuesto(Impuesto impuesto) {
+        this.impuesto = impuesto;
+    }
+
+    public void setMonto(float monto) {
+        this.monto = monto;
+    }
+
+    public int getIdRetencion() {
+        return idRetencion;
+    }
+
+    public Impuesto getImpuesto() {
+        return impuesto;
+    }
+
+    public float getMonto() {
+        return monto;
+    }
+
+    public float calcularRetencion(){
+            return 0;
+        }
+    }
+
