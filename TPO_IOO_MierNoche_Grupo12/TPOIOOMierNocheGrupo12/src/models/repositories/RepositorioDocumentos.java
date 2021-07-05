f3072761dbcb43a40774812eff2628408174b8d0
erledesma <66388868+erledesma@users.noreply.github.com>
2021-07-05 20:24:28 -0300

    facturas en dia

--- TPO_IOO_MierNoche_Grupo12/TPOIOOMierNocheGrupo12/src/models/repositories/RepositorioDocumentos.java
+++ TPO_IOO_MierNoche_Grupo12/TPOIOOMierNocheGrupo12/src/models/repositories/RepositorioDocumentos.java
@@ -26,15 +26,7 @@ public class RepositorioDocumentos extends Repositorio<Documento>{
     }
 
     public List<Documento> facturasEmitdasElDia(LocalDate unDia) {
-        List<Documento> facturas = new ArrayList<>();
-        List<Documento> documentos= this.getElementos().stream().
+        return this.getElementos().stream().
                 filter(e -> e.getFecha().equals(unDia)).collect(Collectors.toList());
-
-        documentos.forEach(documento -> {
-            if (documento.getTipoDocumento().equals(TipoDocumento.Factura)) {
-                facturas.add(documento);
-            }});
-
-        return facturas;
     }
 }
